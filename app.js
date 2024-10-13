8b566a9e015860520e2f0d35b656bc311c20c5b7
Náthany Amabille <133970235+Casagrande01@users.noreply.github.com>
2024-10-13 19:22:53 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var path = require('path');
+const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
@@ -7,22 +7,27 @@ const rotaUsuario = require('./routes/usuario');
 const rotaProduto = require('./routes/produto');
 const rotaIndex = require('./routes/index');
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-
 app.use('/usuario', rotaUsuario);
 app.use('/produto', rotaProduto);
 app.use('/', rotaIndex);
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
